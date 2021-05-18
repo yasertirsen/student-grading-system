0082f82e8f631fb7f14ac1fd5e3ad3fa720437a9
Yacer <mryaser8@gmail.com>
2021-05-18 14:39:51 +0100

    created Rubric, Criterion and Grade

---
+++ src/main/java/model/Grade.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Grade {
+    private String student;
+    private Map<String, Double> scores;
+}
---
+++ src/main/java/model/Rubric.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Rubric {
+    private String name;
+    private List<Criterion> criteria;
+}
